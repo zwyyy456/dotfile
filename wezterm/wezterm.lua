b55ba56df23810616fc12dfca3368bd76e95c12f
zwyyy456 <zwyyy456@hotmail.com>
2023-07-30 15:54:00 +0800

    change wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,21 +10,29 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-config.color_scheme = "Catppuccin Mocha"
-config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
--- config.font = wezterm.font_with_fallback({
--- "Monaco Nerd Font Mono",
--- "Moncao",
--- "PingFang SC",
--- })
-config.font = wezterm.font_with_fallback({
-	{ family = "Monaco Nerd Font Mono", harfbuzz_features = { "liga=0" } },
-	"PingFang SC",
-})
+config.color_scheme = "Mariana"
 config.font_size = 13.0
+if wezterm.target_triple == "aarch64-apple-darwin" then
+	-- This is where you actually apply your config choices
 
--- and finally, return the configuration to wezterm
+	-- For example, changing the color scheme:
+	config.default_prog = { "fish" }
+	-- config.font = wezterm.font_with_fallback({
+	-- "Monaco Nerd Font Mono",
+	-- "Moncao",
+	-- "PingFang SC",
+	-- })
+	config.font = wezterm.font_with_fallback({
+		{ family = "Monaco Nerd Font Mono", harfbuzz_features = { "liga=0" } },
+		"PingFang SC",
+	})
+
+	-- and finally, return the configuration to wezterm
+elseif wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.default_prog = { "pwsh" }
+	config.font = wezterm.font_with_fallback({
+		{ family = "FuraMono NFM", harfbuzz_features = { "liga=0" } },
+		"Microsoft Yahei",
+	})
+end
 return config
