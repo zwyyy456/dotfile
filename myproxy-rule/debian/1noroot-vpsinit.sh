d47ae93baea9ac98ce54e4450ae72fa7eda5cdcf
zwyyy456 <zwyyy456@hotmail.com>
2024-01-02 17:54:38 +0800

    mac, change caddyfile

--- myproxy-rule/debian/1noroot-vpsinit.sh
+++ myproxy-rule/debian/1noroot-vpsinit.sh
@@ -11,22 +11,10 @@ usermod -aG sudo zwyyy
 
 sudo apt install git
 
-<<<<<<< HEAD
-git clone git@github.com:zwyyy456/dotfile.git ~/.config
-
-# 安装 docker
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-=======
 su zwyyy
 chsh -s /usr/bin/fish
 ssh-keygen -t ed25519 -C "zwyyy456@hotmail.com"
 cat ~/.ssh/id_ed25519.pub
->>>>>>> e55a8902d3fb9315dc2343bcab35ed9074fb014a
 
 rm -rf ~/.config
 git clone git@github.com:zwyyy456/dotfile.git ~/.config
