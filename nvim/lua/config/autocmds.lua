3e6c44ac44ed140c1630a7d13abc9b66a62297e0
zwyyy456 <zwyyy456@m1pro>
2025-10-09 23:15:58 +0800

    fix: 使用 kickstart 重新自定义 neovim 配置，不再使用 lazyvim
    1. 设置一些自定义选项
    2. 添加一部分自定义快捷键映射
    3. 添加高亮复制内容的 autocmd

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- 复制文字时高亮
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,50 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- vim.opt 默认表示 global
+-- vim.opt.local 表示 buffer 局部
+-- vim.opt.winlocal 表示 window 局部
 local opt = vim.opt
+
+-- 是否使用 nerd font，许多插件会读取这个变量
+-- 测试为 false 的效果
+vim.g.have_nerd_font = false
+
+-- 显示行号并使用相对行号
+opt.number = true
+opt.relativenumber = true
+
+-- 不显示当前模式，statusline 中会显示
+opt.showmode = false
+
+-- 使 neovim 的剪贴板与 系统保持一致
+-- schedule 函数表示让 vim 先启动，再执行内部操作
+-- 防止影响启动速度
+vim.schedule(function()
+  opt.clipboard = "unnamedplus"
+end)
+
+-- 智能缩进，启用 wrap 后，软换行保持相同的缩进
+opt.breakindent = true
+
+-- 保存 undo 历史
+opt.undofile = true
+
+-- 搜索忽略大小写，当搜索内容包含大写时不忽略
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- 始终显示最左侧的标志列，例如断点、git 更改标记等
+vim.o.signcolumn = "yes"
+
+-- 设置后台事件如 LSP 诊断的触发时长
+-- 表示 vim 停止输入后，将 buffer 更改写入磁盘的等待时间
+vim.o.updatetime = 500
+
+-- 键盘映射的等待时间，保持默认的 1000ms
+vim.o.timeoutlen = 1000
+
+-- 默认为向右与向下分屏
+vim.o.splitright = true
+vim.o.splitbelow = true
+
 -- 缩进
 opt.tabstop = 4
 opt.shiftwidth = 4
