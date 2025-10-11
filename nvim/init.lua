b430419e1a1e838d8cc7cce4b40bb25bbe950afd
zwyyy456 <zwyyy456@m1pro>
2025-10-11 22:23:49 +0800

    feat: 重构 lazy.nvim 初始化逻辑，区分 vscode 和普通模式下的插件加载

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+require("config.options")
 if vim.g.vscode then
   require("vsc.config.keymaps")
+  require("lazy").setup("plugins.simple")
+  require("lazy").setup("plugins.treesitter")
+else
+  require("lazy").setup("plugins")
 end
-require("config.lazy")
