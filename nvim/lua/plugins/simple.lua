ae67ed3918ebd4d6c11ba6db83ceab0f00bf9cc5
zwyyy456 <zwyyy456@m1pro>
2025-10-11 21:16:44 +0800

    feat: 安装 mini.ai、flash.nvim、mini.surround 等几个基础插件，可以在 vscode.neovim 中也运行。
    1. mini.ai: 将 vim 的 text-object 从单词扩充到代码块、函数、类、tag 等
    2. flash.nvim: 按下 s + "text" 之后可以快速跳转到匹配项
    3. mini.surround: 添加为 text-object 快速添加或删除或修改括号等符号的能力

---
+++ nvim/lua/plugins/simple.lua
@@ -0,0 +1,75 @@
+return {
+  {
+    "nvim-mini/mini.ai",
+    event = "VeryLazy",
+    opts = function()
+      local ai = require("mini.ai")
+      return {
+        n_lines = 500,
+        custom_textobjects = {
+          -- yao 复制代码块
+          o = ai.gen_spec.treesitter({ -- code block
+            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+          }),
+          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+          t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+          d = { "%f[%d]%d+" }, -- digits
+          e = { -- Word with case
+            { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
+            "^().*()$",
+          },
+          g = ai.gen_spec.buffer_spec,
+          u = ai.gen_spec.function_call(), -- u for "Usage"
+          U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
+        },
+      }
+    end,
+    -- config = function(_, opts)
+    --   require("mini.ai").setup(opts)
+    --   LazyVim.on_load("which-key.nvim", function()
+    --     vim.schedule(function()
+    --       LazyVim.mini.ai_whichkey(opts)
+    --     end)
+    --   end)
+    -- end,
+  },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    vscode = true,
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      -- 按下 s 后根据后续的输入进行搜索，根据屏幕上显示的字母，按下可以跳转，类似 vimium 插件的 f
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      -- 按下 S 后，屏幕上显示 treesitter 的元素对应的字母，按下可以跳转，类似 vimium 插件的 f
+      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      -- 操作符待决模式，可以用过 yr 来复制 
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      -- 用于 / 与 ? 进行搜索时，例如 /error，再按下 <M-n> 可以显示搜索结果的快捷跳转字母
+      { "<M-n>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      -- Simulate nvim-treesitter incremental selection
+      { "<c-space>", mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter({
+            actions = {
+              ["<c-space>"] = "next",
+              ["<BS>"] = "prev"
+            }
+          }) 
+        end, desc = "Treesitter Incremental Selection" },
+    },
+  },
+  {
+    -- Add/delete/replace surroundings (brackets, quotes, etc.)
+    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+    -- - sd'   - [S]urround [D]elete [']quotes
+    -- - sr)'  - [S]urround [R]eplace [)] [']
+    'nvim-mini/mini.surround',
+    event = "BufReadPost",
+  },
+}
