70659ba07d76a5dfe38accd984a52fcda5cf85cb
zwyyy456 <zwyyy456@m1pro>
2025-10-11 22:23:43 +0800

    feat: 更新基础配置选项，启用 nerd font 并优化缩进和拼写检查设置

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,7 +5,7 @@ local opt = vim.opt
 
 -- 是否使用 nerd font，许多插件会读取这个变量
 -- 测试为 false 的效果
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- 显示行号并使用相对行号
 opt.number = true
@@ -28,27 +28,31 @@ opt.breakindent = true
 opt.undofile = true
 
 -- 搜索忽略大小写，当搜索内容包含大写时不忽略
-vim.o.ignorecase = true
-vim.o.smartcase = true
+opt.ignorecase = true
+opt.smartcase = true
 
 -- 始终显示最左侧的标志列，例如断点、git 更改标记等
-vim.o.signcolumn = "yes"
+opt.signcolumn = "yes"
 
 -- 设置后台事件如 LSP 诊断的触发时长
 -- 表示 vim 停止输入后，将 buffer 更改写入磁盘的等待时间
-vim.o.updatetime = 500
+opt.updatetime = 500
 
 -- 键盘映射的等待时间，保持默认的 1000ms
-vim.o.timeoutlen = 1000
+opt.timeoutlen = 1000
 
 -- 默认为向右与向下分屏
-vim.o.splitright = true
-vim.o.splitbelow = true
+opt.splitright = true
+opt.splitbelow = true
 
 -- 缩进
+opt.expandtab = true
+opt.softtabstop = 4
 opt.tabstop = 4
 opt.shiftwidth = 4
+
 opt.fileformat = "unix"
+opt.spelllang = { "en", "cjk" }
 
 function my_paste(reg)
   return function(lines)
@@ -74,4 +78,3 @@ if vim.env.SSH_TTY then
     },
   }
 end
-opt.spelllang = { "en", "cjk" }
