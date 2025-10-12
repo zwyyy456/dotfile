ad1ba92a480d3d3ab8d913f6d618139b4f325f12
zwyyy456 <zwyyy456@m1pro>
2025-10-12 15:29:47 +0800

    feat: 统一使用 vim.keymap.set 替代 LazyVim.safe_keymap_set，并优化窗口移动键位为大写字母，snacks 相关的快捷键在 snacks.lua 中配置

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,27 +1,28 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local map = LazyVim.safe_keymap_set
+-- local map = LazyVim.safe_keymap_set
+local map = vim.keymap.set
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 map("i", "jk", "<ESC>")
 
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+map("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- NOTE: 两次 esc 使终端从 insert 模式切换回 normal 模式
 -- 在某些终端不一定生效
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+map("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- 将当前文件的 LSP diagnostic 加载到（Location List）中，并显示
-vim.keymap.set("n", "<leader>qf", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+map("n", "<leader>qf", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- 使用 <Meta> hjkl 来在窗口间移动
-map("n", "<M-h>", "<C-w>H", { desc = "Go to Left Window", remap = true })
-map("n", "<M-j>", "<C-w>J", { desc = "Go to Lower Window", remap = true })
-map("n", "<M-k>", "<C-w>K", { desc = "Go to Upper Window", remap = true })
-map("n", "<M-l>", "<C-w>L", { desc = "Go to Right Window", remap = true })
+map("n", "<M-H>", "<C-w>H", { desc = "Go to Left Window", remap = true })
+map("n", "<M-J>", "<C-w>J", { desc = "Go to Lower Window", remap = true })
+map("n", "<M-K>", "<C-w>K", { desc = "Go to Upper Window", remap = true })
+map("n", "<M-L>", "<C-w>L", { desc = "Go to Right Window", remap = true })
 
 -- 使用 <Ctrl> hjkl 作为上下左右移动
 map({ "n", "x", "i" }, "<C-h>", "<Left>")
@@ -29,12 +30,3 @@ map({ "n", "x", "i" }, "<C-j>", "<Down>")
 map({ "n", "x", "i" }, "<C-k>", "<Up>")
 map({ "n", "x", "i" }, "<C-l>", "<Right>")
 
--- fzf-lua 通过 lsp 搜索当前文档的所有符号
-map("n", "<leader>fn", function()
-  Snacks.picker.lsp_symbols()
-end, { desc = "Snacks.picker: Document Symbols", silent = true })
-
--- 格式化代码
-map({ "n", "v" }, "<leader>fm", function()
-  LazyVim.format({ force = true })
-end, { desc = "Format" })
