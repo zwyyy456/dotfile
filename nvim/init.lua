79bb121bef2afe802d33f8d5a880962debe46b8b
zwyyy456 <zwyyy456@m1pro>
2025-10-12 16:00:14 +0800

    feat: 添加全局快捷键配置并统一在 init.lua 中加载

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.options")
+require("config.keymaps")
 if vim.g.vscode then
   require("vsc.config.keymaps")
-  require("lazy").setup("plugins.simple")
-  require("lazy").setup("plugins.treesitter")
+  require("lazy").setup({
+    require("plugins.simple"),
+    require("plugins.treesitter")
+  })
 else
   require("lazy").setup("plugins")
 end
