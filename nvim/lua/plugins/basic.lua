0ca8eeadcbd253f47e6ee51e05506f0485403c44
zwyyy456 <zwyyy456@m1pro>
2025-10-12 16:00:07 +0800

    feat: 重构 gitsigns.nvim 配置，添加更多 git 操作快捷键并优化符号显示，同时统一其他插件的字符串引号和格式

--- nvim/lua/plugins/basic.lua
+++ nvim/lua/plugins/basic.lua
@@ -2,39 +2,73 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     event = "BufReadPost",
-    opts = function()
-      -- 创建切换 git 修改标记展示的快捷键
-      Snacks.toggle({
-        name = "Git Signs",
-        get = function()
-          return require("gitsigns.config").config.signcolumn
-        end,
-        set = function(state)
-          require("gitsigns").toggle_signs(state)
-        end,
-      }):map("<leader>uG")
-      -- 返回静态配置表格，这个表格会传递给 gitsigns.setup()
-      return {
-        signs = {
-          add = { text = '+' },
-          change = { text = '~' },
-          delete = { text = '_' },
-          topdelete = { text = '‾' },
-          changedelete = { text = '~' },
-        },
-      }
-    end,
+    opts = {
+      signs = {
+        add = { text = "┃" },
+        change = { text = "┃" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked = { text = "┆" },
+      },
+      signs_staged = {
+        add = { text = "┃" },
+        change = { text = "┃" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked = { text = "┆" },
+      },
+      signs_staged_enable = true,
+      signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+      on_attach = function(buffer)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, desc)
+          vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc, silent = true })
+        end
+
+      -- stylua: ignore start
+      map("n", "]h", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "]c", bang = true })
+        else
+          gs.nav_hunk("next")
+        end
+      end, "Next Hunk")
+      map("n", "[h", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "[c", bang = true })
+        else
+          gs.nav_hunk("prev")
+        end
+      end, "Prev Hunk")
+      map("n", "]H", function() gs.nav_hunk("last") end, "Last Hunk")
+      map("n", "[H", function() gs.nav_hunk("first") end, "First Hunk")
+      map({ "n", "v" }, "<leader>Ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+      map({ "n", "v" }, "<leader>Ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+      map("n", "<leader>GhS", gs.stage_buffer, "Stage Buffer")
+      map("n", "<leader>Ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+      map("n", "<leader>GhR", gs.reset_buffer, "Reset Buffer")
+      map("n", "<leader>Ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
+      map("n", "<leader>Ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
+      map("n", "<leader>GhB", function() gs.blame() end, "Blame Buffer")
+      map("n", "<leader>Ghd", gs.diffthis, "Diff This")
+      map("n", "<leader>GhD", function() gs.diffthis("~") end, "Diff This ~")
+      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+      end,
+    },
   },
   {
     "nvim-mini/mini.pairs",
-    event = "VeryLazy"
+    event = "VeryLazy",
   },
   -- 高亮 Todo 等注释
   {
     "folke/todo-comments.nvim",
     cmd = { "TodoTrouble", "TodoTelescope" },
     event = "FileType",
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { "nvim-lua/plenary.nvim" },
     opts = {},
     -- stylua: ignore
     keys = {
@@ -44,12 +78,12 @@ return {
       { "<leader>TT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
       -- 配置使用 snacks.picker 来搜索 todo 的快捷键
       { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
-      { "<leader>sT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc},
+      { "<leader>sT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
       { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
     },
   },
   -- nvim 自带注释语法增强
-  {"folke/ts-comments.nvim", event = "VeryLazy", opts = {}},
+  { "folke/ts-comments.nvim", event = "VeryLazy", opts = {} },
   {
     -- 保存会话，可以通过 <leader>ps 来恢复会话
     "folke/persistence.nvim",
@@ -63,5 +97,5 @@ return {
       { "<leader>pd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
     },
   },
-  { "nvim-lua/plenary.nvim", lazy = true }
+  { "nvim-lua/plenary.nvim", lazy = true },
 }
